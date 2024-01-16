4b832b519af7cec6c73287d684cb7ca0094d17af
seblful <alesha.chimba@gmail.com>
2024-01-16 11:54:59 +0300

    [EDIT] add data path, dataset, trainer

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+from dataset import CancerDataset
 from unet import UNet
+from trainer import UnetTrainer
 import torch
+
+import os
+HOME = os.getcwd()
+DATA = os.path.join(HOME, 'data')
 
 
 def main():
-    unet = UNet(in_channels=3,
-                out_channels=3)
-    data = torch.randn(size=(1, 3, 572, 572))
+    # unet = UNet(in_channels=3,
+    #             out_channels=3)
+    # data = torch.randn(size=(1, 3, 572, 572))
 
-    x = unet(data)
+    # x = unet(data)
+
+    d = CancerDataset(data_path=DATA)
+    # print(d[0]['image'].shape, d[0]['mask'].shape)
+
+    t = UnetTrainer(dataset=d,
+                    val_ratio=0.1,
+                    batch_size=8,
+                    num_epochs=5)
+
+    t
 
 
 if __name__ == "__main__":
