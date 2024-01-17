fc0df45d207abb7a628577213b4cfa9174d7ce3b
seblful <alesha.chimba@gmail.com>
2024-01-17 10:09:54 +0300

    [EDIT] minor changes

--- main.py
+++ main.py
@@ -10,23 +10,16 @@ CHECKPOINTS = os.path.join(HOME, 'checkpoints')
 
 
 def main():
-    # unet = UNet(in_channels=3,
-    #             out_channels=3)
-    # data = torch.randn(size=(1, 3, 572, 572))
+    cancer_dataset = CancerDataset(data_path=DATA)
 
-    # x = unet(data)
+    unet_trainer = UnetTrainer(dataset=cancer_dataset,
+                               val_ratio=0.1,
+                               batch_size=2,
+                               num_epochs=5,
+                               checkpoints_path=CHECKPOINTS,
+                               amp=True)
 
-    d = CancerDataset(data_path=DATA)
-    # print(d[0]['image'].shape, d[0]['mask'].shape)
-
-    t = UnetTrainer(dataset=d,
-                    val_ratio=0.1,
-                    batch_size=2,
-                    num_epochs=5,
-                    checkpoints_path=CHECKPOINTS,
-                    amp=True)
-
-    t.train()
+    unet_trainer.train()
 
 
 if __name__ == "__main__":
