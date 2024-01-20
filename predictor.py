c923d0d1d502d7d05bdf2ee5ccebf8dbe3227b70
seblful <alesha.chimba@gmail.com>
2024-01-20 13:28:58 +0300

    [EDIT] predict with out thresh

--- predictor.py
+++ predictor.py
@@ -36,23 +36,23 @@ class UnetPredictor():
 
     def predict(self,
                 image,
-                out_threshold=0.001):
+                out_threshold=0.5):
         # Transform image
         transf_image = self.transform_image(image)
 
         with torch.no_grad():
             output = self.net(transf_image).cpu()
-            mask = F.sigmoid(output)
-            mask, _ = torch.max(mask, dim=0)
+            mask = torch.sigmoid(output)
+            mask = mask > out_threshold
             mask = mask.view(512, 512)
 
-        return transf_image, mask
+        return mask
 
     def plot_mask(self, image, mask):
         fig, ax = plt.subplots(1, 2)
         ax[0].set_title('Input Image')
         ax[0].imshow(image)
         ax[1].set_title('Mask')
-        ax[1].imshow(mask[:, :], cmap='gray')
+        ax[1].imshow(mask, cmap='gray')
         plt.xticks([]), plt.yticks([])
         plt.show()
